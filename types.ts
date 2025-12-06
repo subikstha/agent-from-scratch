c76d6c3f0bcf72e1b1f7f7954bb96cd91dee1aed
subikstha <subsatch@gmail.com>
2025-12-06 17:14:47 +0545

    Prepared memory for adding context to future conversations

--- types.ts
+++ types.ts
@@ -1,6 +1,9 @@
+import type { ChatCompletionMessage } from 'groq-sdk/resources/chat.mjs'
+
 export type AIMessage =
   | { role: 'user'; content: string }
   | { role: 'tool'; content: string; tool_call_id: string }
+  | { role: 'assistant'; content: ChatCompletionMessage }
 
 export interface ToolFn<A = any, T = any> {
   (input: { userMessage: string; toolArgs: A }): Promise<T>
